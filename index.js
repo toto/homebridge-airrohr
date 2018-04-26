4bf2cd5e199f513c51f071c2ad800f77f29ee0e0
Alexander Ryden <alexander.ryden@gmail.com>
2018-04-26 08:06:30 +0200

    Remove unused function

--- index.js
+++ index.js
@@ -19,41 +19,6 @@ module.exports = function(homebridge) {
   );
 };
 
-function getCurrentSensorData(jsonURL, callback) {
-  http.get(jsonURL, (res) => {
-    const { statusCode } = res;
-    const contentType = res.headers['content-type'];
-
-    let error;
-    if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +
-                        `Status Code: ${statusCode}`);
-    } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +
-                        `Expected application/json but received ${contentType}`);
-    }
-    if (error) {
-      res.resume();
-      callback(null, error);
-      return;
-    }
-
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        callback(parsedData, null);
-      } catch (e) {
-        callback(null, e);
-      }
-    });
-  }).on('error', (e) => {
-    callback(null, e);
-  });
-};
-
 function AirRohrAccessory(log, config) {
     this.category = Accessory.Categories.SENSOR;
     this.log = log;
